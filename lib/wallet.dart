92afc0da1eabb0688cf26adb393b45e265f623ad
luiz-hms <luiz.henriquems@hotmail.com>
2025-09-20 16:10:23 -0300

    wallet

--- lib/wallet.dart
+++ lib/wallet.dart
@@ -58,4 +58,80 @@ extension Wallet on TwoFinanceBlockchain {
       throw Exception('failed to get state: $e');
     }
   }
+
+  Future<ContractOutput> transferWallet({
+  required String to,
+  required String amount,
+  int decimals = 0,
+}) async {
+  final String from = _activePublicKey ?? '';
+  if (from.isEmpty) throw ArgumentError('public key not set');
+
+  if (to.isEmpty) throw ArgumentError('to address not set');
+  if (to == from) throw ArgumentError('cannot transfer to the same address');
+  KeyManager.validateEdDSAPublicKey(to);
+
+  if (amount.isEmpty) throw ArgumentError('amount not set');
+
+  // Ajusta a quantidade se houver casas decimais
+  String finalAmount = amount;
+  if (decimals != 0) {
+    try {
+      finalAmount = DecimalRescaler.rescaleString(amount, 0, decimals);
+    } catch (e) {
+      throw Exception('failed to convert amount to big int: $e');
+    }
+  }
+
+  const String contractVersion = WALLET_CONTRACT_V1;
+  const String method = METHOD_TRANSFER_WALLET;
+  final Map<String, dynamic> data = {
+    "amount": finalAmount,
+    "from": from,
+    "to": to,
+  };
+
+  final DateTime timestamp = DateTime.now().toUtc();
+
+  // Recupera nonce e incrementa
+  int nonce;
+  try {
+    nonce = await getNonce(from);
+    nonce += 1;
+  } catch (e) {
+    throw Exception('failed to get nonce: $e');
+  }
+
+  // Cria a transação
+  final newTx = Transaction.create(
+    from: from,
+    to: to,
+    timestamp: timestamp,
+    contractVersion: contractVersion,
+    method: method,
+    data: data,
+    nonce: nonce,
+  );
+
+  final tx = newTx.get();
+  late String txSigned;
+  try {
+    txSigned = signTransaction(_activePrivateKey!, tx) as String;
+  } catch (e) {
+    throw Exception('failed to sign transaction: $e');
+  }
+
+  // Envia a transação
+  try {
+    await handlerRequest(REQUEST_METHOD_SEND_TRANSACTION, txSigned, _replyTo);
+  } catch (e) {
+    throw Exception('failed to send transaction: $e');
+  }
+
+  // TODO: Mapear o ContractOutput como no Go (Transfer, Events, WalletSender/Receiver, etc.)
+  final contractOutput = ContractOutput();
+
+  return contractOutput;
+}
+
 }
