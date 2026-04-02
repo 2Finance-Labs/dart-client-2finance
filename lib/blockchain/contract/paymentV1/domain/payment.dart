3c758f9d05c16d8c63aaf64bf2d2f0858d9daa77
Kaique Sousa <kaiquecapt@gmail.com>
2026-04-02 08:43:13 -0300

    feat: adicionando tests e2e de payment

---
+++ lib/blockchain/contract/paymentV1/domain/payment.dart
@@ -0,0 +1,13 @@
+const String PAYMENT_CREATED_LOG = 'Payment_Created';
+const String PAYMENT_AUTHORIZED_LOG = 'Payment_Authorized';
+const String PAYMENT_CAPTURED_LOG = 'Payment_Captured';
+const String PAYMENT_REFUNDED_LOG = 'Payment_Refunded';
+const String PAYMENT_VOIDED_LOG = 'Payment_Voided';
+const String PAYMENT_PAUSED_LOG = 'Payment_Paused';
+const String PAYMENT_UNPAUSED_LOG = 'Payment_Unpaused';
+
+const String STATUS_CREATED = 'created';
+const String STATUS_AUTHORIZED = 'authorized';
+const String STATUS_CAPTURED = 'captured';
+const String STATUS_REFUNDED = 'refunded';
+const String STATUS_VOIDED = 'voided';
